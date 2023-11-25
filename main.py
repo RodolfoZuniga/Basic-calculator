c6e69fe58170b7d2d29299a2a9d5555fa92a442f
Rodolfopz <rjunior007@live.com.mx>
2023-11-25 15:46:48 -0600

    Bienvenidos :D

---
+++ main.py
@@ -0,0 +1,36 @@
+def calculadora():
+    print("Calculadora básica")
+    print("Operaciones disponibles:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Exponencial")
+
+    
+    opcion = int(input("Seleccione la operación (1-5): "))
+
+    
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    resultado = 0
+
+    
+    if opcion == 1:
+        resultado = suma(num1, num2)
+    elif opcion == 2:
+        resultado = resta(num1, num2)
+    elif opcion == 3:
+        resultado = multiplicacion(num1, num2)
+    elif opcion == 4:
+        resultado = division(num1, num2)
+    elif opcion == 5:
+        resultado = exponencial(num1, num2)
+    else:
+        print("Opción no válida.")
+
+    
+    print("El resultado es:", resultado)
+
+calculadora()
